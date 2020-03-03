c93105f20b9eb2d37e93eeb22bf87ba6d95fde13
dinithchanuka <dinithchanuka22@gmail.com>
2020-03-03 08:20:15 +0530

    new practices

---
+++ shedulling-task/src/main/java/com/example/shedullingtask/ShedullingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.shedullingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShedullingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShedullingTaskApplication.class, args);
+	}
+
+}
---
+++ shedulling-task/src/test/java/com/example/shedullingtask/ShedullingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shedullingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShedullingTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
